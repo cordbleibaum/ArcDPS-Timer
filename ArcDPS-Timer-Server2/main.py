724c2dc442fd4995cc5ca7d17a550404fe0189b9
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-04 00:58:18 +0200

    options for new server

--- ArcDPS-Timer-Server2/main.py
+++ ArcDPS-Timer-Server2/main.py
@@ -1,6 +1,12 @@
 import tornado.ioloop
 import tornado.web
 
+from tornado.options import define, options, parse_command_line
+
+define("port", default=5000, help="run on the given port", type=int)
+define("debug", default=True, help="run in debug mode")
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write({
@@ -9,14 +15,17 @@ class MainHandler(tornado.web.RequestHandler):
         })
 
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+        ],
+        debug=options.debug
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(5000)
-    tornado.ioloop.IOLoop.current().start()
-
+    main()
