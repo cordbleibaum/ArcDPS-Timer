3ac124b3df7a7d134d9fcbcb4244cdfe8d5ff836
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-04 01:49:23 +0200

    call periodic cleanup

--- ArcDPS-Timer-Server2/main.py
+++ ArcDPS-Timer-Server2/main.py
@@ -1,10 +1,22 @@
-import tornado.ioloop
-import tornado.web
+import logging
+from datetime import datetime
+from typing import Dict
 
+import tornado.web
+import tornado.locks
+import tornado.ioloop
 from tornado.options import define, options, parse_command_line
 
 define("port", default=5000, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
+
+
+class GroupStatus(object):
+    def __init__(self):
+        self.update_lock = tornado.locks.Condition()
+
+
+global_groups : Dict[str, GroupStatus] = {}
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -16,34 +28,40 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 class StartHandler(tornado.web.RequestHandler):
-    def post(self, group_id):
+    def post(self, group_id: str):
         pass
 
 
 class StopHandler(tornado.web.RequestHandler):
-    def post(self, group_id):
+    def post(self, group_id: str):
         pass
 
 
 class ResetHandler(tornado.web.RequestHandler):
-    def post(self, group_id):
+    def post(self, group_id: str):
         pass
 
 
 class PrepareHandler(tornado.web.RequestHandler):
-    def post(self, group_id):
+    def post(self, group_id: str):
         pass
 
 
 class StatusHandler(tornado.web.RequestHandler):
-    async def post(self, group_id):
-        pass
+    async def post(self, group_id: str):
+        last_update = self.get_argument("last_update", datetime.fromtimestamp(0))
 
     def on_connection_close(self):
-        pass
+        self.update_future.cancel()
+
+
+def cleanupGroups():
+    logging.info("Running cleanup")
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting server")
     parse_command_line()
     app = tornado.web.Application(
         [
@@ -57,7 +75,13 @@ def main():
         debug=options.debug
     )
     app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+   
+    cleanupTimer = 1000*60*60
+    cleanupCallback = tornado.ioloop.PeriodicCallback(callback=cleanupGroups, callback_time=cleanupTimer)
+    cleanupCallback.start()
+
+    io_loop = tornado.ioloop.IOLoop.current()
+    io_loop.start()
 
 
 if __name__ == "__main__":
