ac8b290ea2113715f8833a85c405f818b3648275
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-04 01:10:47 +0200

    handlers for server methods

--- ArcDPS-Timer-Server2/main.py
+++ ArcDPS-Timer-Server2/main.py
@@ -15,11 +15,44 @@ class MainHandler(tornado.web.RequestHandler):
         })
 
 
+class StartHandler(tornado.web.RequestHandler):
+    def post(self, group_id):
+        pass
+
+
+class StopHandler(tornado.web.RequestHandler):
+    def post(self, group_id):
+        pass
+
+
+class ResetHandler(tornado.web.RequestHandler):
+    def post(self, group_id):
+        pass
+
+
+class PrepareHandler(tornado.web.RequestHandler):
+    def post(self, group_id):
+        pass
+
+
+class StatusHandler(tornado.web.RequestHandler):
+    async def post(self, group_id):
+        pass
+
+    def on_connection_close(self):
+        pass
+
+
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
             (r"/", MainHandler),
+            (r"/groups/([^/]+)/", StatusHandler),
+            (r"/groups/([^/]+)/start", StartHandler),
+            (r"/groups/([^/]+)/stop", StopHandler),
+            (r"/groups/([^/]+)/prepare", PrepareHandler),
+            (r"/groups/([^/]+)/reset", ResetHandler),
         ],
         debug=options.debug
     )
