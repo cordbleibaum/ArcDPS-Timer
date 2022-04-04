cc8a0dde124eb87f63a242ac33b22438c42c706e
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-05 00:27:57 +0200

    Lock group to change it

--- ArcDPS-Timer-Server2/main.py
+++ ArcDPS-Timer-Server2/main.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime, timedelta
+from enum import Enum
 
 import tornado.web
 import tornado.locks
@@ -9,6 +10,13 @@ from tornado.options import define, options, parse_command_line
 
 define("port", default=5000, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
+
+
+class TimerStatus(Enum):
+     stopped = 0
+     started = 1
+     prepared = 2
+     resetted = 3
 
 
 class SegmentStatus(object):
@@ -24,20 +32,35 @@ class SegmentStatus(object):
 class GroupStatus(object):
     def __init__(self):
         self.update_lock = tornado.locks.Condition()
-        self.last_update = datetime.now()
-        self.start_time = datetime.now()
-        self.stop_time = datetime.now()
-        self.status = 'stopped'
+        self.last_update = datetime.fromtimestamp(0)
+        self.start_time = datetime.fromtimestamp(0)
+        self.stop_time = datetime.fromtimestamp(0)
+        self.status = TimerStatus.stopped
         self.segments : list[SegmentStatus] = []
+        self.changeSemaphore = tornado.locks.Semaphore(1)
 
 
 global_groups : dict[str, GroupStatus] = {}
 
 
 class JsonHandler(tornado.web.RequestHandler):
-    def prepare(self):
+    def prepare(self) -> None:
         if self.request.headers['Content-Type'] == 'application/json':
             self.args = tornado.escape.json_decode(self.request.body)
+
+
+class GroupModifyHandler(JsonHandler):
+    async def prepare(self) -> None:
+        super().prepare()
+        self.status = GroupStatus()
+        group_id = self.path_args[0]
+        if group_id in global_groups:
+            self.status = global_groups[group_id]
+        await self.status.changeSemaphore.acquire()
+
+    def on_finish(self) -> None:
+        self.status.changeSemaphore.release()
+        return super().on_finish()
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -48,23 +71,23 @@ class MainHandler(tornado.web.RequestHandler):
         })
 
 
-class StartHandler(JsonHandler):
-    def post(self, group_id: str):
+class StartHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
-class StopHandler(JsonHandler):
-    def post(self, group_id: str):
+class StopHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
-class ResetHandler(JsonHandler):
-    def post(self, group_id: str):
+class ResetHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
-class PrepareHandler(JsonHandler):
-    def post(self, group_id: str):
+class PrepareHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
@@ -76,13 +99,13 @@ class StatusHandler(JsonHandler):
         self.update_future.cancel()
 
 
-class SegmentHandler(JsonHandler):
-    def post(self, group_id: str):
+class SegmentHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
-class ClearSegmentsHandler(JsonHandler):
-    def post(self, group_id: str):
+class ClearSegmentsHandler(GroupModifyHandler):
+    async def post(self, group_id: str):
         pass
 
 
