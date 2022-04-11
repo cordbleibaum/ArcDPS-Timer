be9252411eda61fd614e6770039f5a0987acf696
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-11 02:42:28 +0200

    check

--- ArcDPS-Timer-Server2/main.py
+++ ArcDPS-Timer-Server2/main.py
@@ -215,6 +215,7 @@ class SegmentHandler(GroupModifyHandler):
 
         shortest_time : timedelta = segment.end - self.group.start_time
         shortest_duration : timedelta = segment.end - segment.start
+        logging.info(self.group.start_time)
 
         if is_new_segment or (shortest_time < segment.shortest_time):
             segment.shortest_time = shortest_time
