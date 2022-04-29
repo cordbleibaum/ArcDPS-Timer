881a522843da75468d516e13e03f58007e6b918c
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-04-29 14:35:53 +0200

    catch ipv6

--- ArcDPS-Timer/maptracker.cpp
+++ ArcDPS-Timer/maptracker.cpp
@@ -1,6 +1,7 @@
 #include "maptracker.h"
 
 #include "hash-library/crc32.h"
+#include "arcdps.h"
 
 MapTracker::MapTracker(GW2MumbleLink& mumble_link)
 : mumble_link(mumble_link) {
@@ -18,7 +19,15 @@ bool MapTracker::watch() {
 		map_id = mumble_link->getMumbleContext()->mapId;
 		
 		std::unique_lock lock(mapcode_mutex);
-		map_code = CRC32()(mumble_link->getMumbleContext()->serverAddress, sizeof(sockaddr_in));
+
+		sockaddr_in* address = (sockaddr_in*) &(mumble_link->getMumbleContext()->serverAddress[0]);
+		if (address->sin_family == AF_INET) {
+			map_code = CRC32()(mumble_link->getMumbleContext()->serverAddress, sizeof(sockaddr_in));
+		}
+		else {
+			log_arc("timer: error: ipv6 is unsupported");
+			return false;
+		}
 		return true;
 	}
 	return false;
