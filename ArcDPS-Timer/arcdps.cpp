6d1cdc2ee612ec0cf85c6d703d94f349c722b441
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2022-06-25 18:42:00 +0200

    use constexpr if instead of preprocessor

--- ArcDPS-Timer/arcdps.cpp
+++ ArcDPS-Timer/arcdps.cpp
@@ -26,9 +26,9 @@ void log(std::string str) {
 }
 
 void log_debug(std::string str) {
-#ifndef NDEBUG
-	log(str);
-#endif // !NDEBUG
+	if constexpr (!NDEBUG) {
+		log(str);
+	}
 }
 
 extern "C" __declspec(dllexport) void* get_init_addr(char* arcversion, ImGuiContext * imguictx, void* id3dptr, HANDLE arcdll, void* mallocfn, void* freefn, uint32_t d3dversion) {
